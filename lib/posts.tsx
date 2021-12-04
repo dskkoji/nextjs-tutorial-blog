e0eb24e2792d959cf76a479623ced0c7b17a1823
dkoji <dkojima.2nd@gmail.com>
2021-12-04 21:21:48 +0900

    Modified typescript

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -4,7 +4,6 @@ import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
 
-
 const postsDirectory = path.join(process.cwd(), 'posts')
 
 export function getSortedPostsData() {
@@ -19,24 +18,21 @@ export function getSortedPostsData() {
 
     return {
       id,
-      ...matterResult.data
+      ...(matterResult.data as { date: string; title: string })
     }
   })
 
-  return allPostsData.sort(({ date: a}, { date: b }) => {
-    if (a < b) {
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
       return 1
-    } else if (a > b) {
-      return -1
     } else {
-      return 0
+      return -1
     }
   })
 }
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory)
-
   return fileNames.map(fileName => {
     return {
       params: {
@@ -46,21 +42,21 @@ export function getAllPostIds() {
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
   const matterResult = matter(fileContents)
 
   const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content)
+      .use(html)
+      .process(matterResult.content)
 
   const contentHtml = processedContent.toString()
 
   return {
     id,
     contentHtml,
-    ...matterResult.data
+    ...(matterResult.data as { date: string; title: string })
   }
 }
---
+++ posts/nextjs.md
@@ -0,0 +1,12 @@
+---
+title: 'My sample Next.js'
+date: '2021-12-04'
+---
+
+test test 
+test test 
+test test 
+test test 
+test test 
+test test 
+test test 
